fb0da00a7176d897a48c15d173186f152ac0ce73
Mohammed Numan <mdnumandocl@gmail.com>
2026-01-22 14:51:12 +0530

    Add User model

---
+++ models/User.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { versionKey: false }
+);
+
+export default mongoose.model("User", userSchema);
