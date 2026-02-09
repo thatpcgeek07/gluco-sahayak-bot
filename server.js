f6bfb95fd6696156d6aa57f2efb0137bff80abcf
saadxli <saadali.aboutme@gmail.com>
2026-02-09 23:15:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+// Enable CORS for local testing with HTML files
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // ========================================
 // CONFIGURATION
 // ========================================
